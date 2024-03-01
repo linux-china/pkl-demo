0d3b824f3fad508858b78c2197ed54e01dce9bc5
linux_china <libing.chen@gmail.com>
2024-03-01 17:21:57 +0800

    chore: add pigeonService

--- src/main/java/org/mvnsearch/PortalController.java
+++ src/main/java/org/mvnsearch/PortalController.java
@@ -1,5 +1,7 @@
 package org.mvnsearch;
 
+import org.mvnsearch.service.PigeonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PortalController {
     @Value("${name}")
     private String name;
+    @Autowired
+    private PigeonService pigeonService;
 
     @GetMapping("/")
     public String index() {
+        System.out.println(pigeonService.getPigeon().getName());
         return "Hello " + name;
     }
 }
