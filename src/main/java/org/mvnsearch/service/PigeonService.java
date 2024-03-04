9e5b9fefe626da810e015ae4fdeacb2f8fe7196f
Libing Chen <libing.chen@gmail.com>
2024-03-04 21:20:49 +0800

    chore: add pigeon

--- src/main/java/org/mvnsearch/service/PigeonService.java
+++ src/main/java/org/mvnsearch/service/PigeonService.java
@@ -1,22 +1,16 @@
 package org.mvnsearch.service;
 
 import org.mvnsearch.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @Service
-@ConfigurationProperties
 public class PigeonService {
-    AppConfig.Pigeon pigeon;
+  @Autowired
+  AppConfig.Pigeon pigeon;
 
-    public void setPigeon(AppConfig.Pigeon pigeon) {
-        this.pigeon = pigeon;
-    }
-
-    public PigeonService() {
-    }
-
-    public AppConfig.Pigeon getPigeon() {
-        return pigeon;
-    }
+  public AppConfig.Pigeon getPigeon() {
+    return pigeon;
+  }
 }
