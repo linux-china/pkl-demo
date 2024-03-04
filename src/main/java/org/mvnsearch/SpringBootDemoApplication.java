cee0c5995a53da4cba32f19e8496acb5092147f3
Libing Chen <libing.chen@gmail.com>
2024-03-04 21:21:44 +0800

    chore: add @ConfigurationPropertiesScan

--- src/main/java/org/mvnsearch/SpringBootDemoApplication.java
+++ src/main/java/org/mvnsearch/SpringBootDemoApplication.java
@@ -2,8 +2,10 @@ package org.mvnsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
